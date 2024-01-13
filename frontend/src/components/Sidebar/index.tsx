d42d1d679343e4651d75d7b1543d025471662e1d
Xlebabutti <110064356+Xlebabutti@users.noreply.github.com>
2024-01-13 15:20:55 +0300

    add mui

--- frontend/src/components/Sidebar/index.tsx
+++ frontend/src/components/Sidebar/index.tsx
@@ -1,16 +1,68 @@
-import './_sidebar.scss'
+// import './_sidebar.scss'
 
+
+// const Sidebar = () => {
+//     return (
+//         <div className='sidebar'>
+//             <div className="sidebar-container">
+//                 <div className="menu-icon">
+//                     menu icon
+//                 </div>
+//                 <div className="porfile">
+//                     <img src="" alt="" />
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default Sidebar
+import { Avatar, Drawer, List, Stack, Toolbar } from "@mui/material";
+import assets from "../../assets";
+import colorConfigs from "../../configs/colorConfigs";
+import sizeConfigs from "../../configs/sizeConfigs";
+import appRoutes from "../../routes/appRoutes";
+import SidebarItem from "./SidebarItem";
+import SidebarItemCollapse from "./SidebarItemCollapse";
 
 const Sidebar = () => {
-    return (
-        <div className='sidebar'>
-            <div className="sidebar-container">
-                <div className="menu-icon">
-                        menu icon
-                </div>
-            </div>
-        </div>
-    );
-}
+  return (
+    <Drawer
+      variant="permanent"
+      sx={{
+        width: sizeConfigs.sidebar.width,
+        flexShrink: 0,
+        "& .MuiDrawer-paper": {
+          width: sizeConfigs.sidebar.width,
+          boxSizing: "border-box",
+          borderRight: "0px",
+          backgroundColor: colorConfigs.sidebar.bg,
+          color: colorConfigs.sidebar.color
+        }
+      }}
+    >
+      <List disablePadding>
+        <Toolbar sx={{ marginBottom: "20px" }}>
+          <Stack
+            sx={{ width: "100%" }}
+            direction="row"
+            justifyContent="center"
+          >
+            <Avatar src={assets.images.logo} />
+          </Stack>
+        </Toolbar>
+        {appRoutes.map((route, index) => (
+          route.sidebarProps ? (
+            route.child ? (
+              <SidebarItemCollapse item={route} key={index} />
+            ) : (
+              <SidebarItem item={route} key={index} />
+            )
+          ) : null
+        ))}
+      </List>
+    </Drawer>
+  );
+};
 
 export default Sidebar;
