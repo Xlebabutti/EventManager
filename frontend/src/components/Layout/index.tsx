fdee14130b01decd7544e85dcc005e79e5cb618a
Xlebabutti <110064356+Xlebabutti@users.noreply.github.com>
2024-01-15 02:52:00 +0300

    add position main element scss

--- frontend/src/components/Layout/index.tsx
+++ frontend/src/components/Layout/index.tsx
@@ -2,11 +2,12 @@ import { Outlet } from "react-router-dom"
 import Header from "../Header"
 import Sidebar from "../Sidebar"
 import Footer from "../Footer"
+import "./_layout.scss"
 
 
 const Layout = () => {
     return (
-        <div className="main">
+        <div className="app">
             <Header />
             <Sidebar />
             <main className="content">
