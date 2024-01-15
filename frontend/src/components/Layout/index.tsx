495b516f82cb927e3fb46425af1dc04423935b73
Xlebabutti <110064356+Xlebabutti@users.noreply.github.com>
2024-01-15 03:44:17 +0300

    fix header + content

--- frontend/src/components/Layout/index.tsx
+++ frontend/src/components/Layout/index.tsx
@@ -1,19 +1,17 @@
 import { Outlet } from "react-router-dom"
 import Header from "../Header"
 import Sidebar from "../Sidebar"
-import Footer from "../Footer"
 import "./_layout.scss"
 
 
 const Layout = () => {
     return (
         <div className="app">
-            <Header />
             <Sidebar />
             <main className="content">
+                <Header />
                 <Outlet />
             </main>
-            <Footer />
         </div>
     )
 }
